4567a35210bc09372a975596c2bc1719c16a103c
Samuel Tan <31382683+samueltan3972@users.noreply.github.com>
2022-11-29 14:38:48 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Getting Started
+
+This is the repository for UM WQD 7003 Data Analytics Group Project.
---
+++ convert.R
@@ -0,0 +1,6 @@
+setwd("D:/UM Master/Sem 1/Data Analytics/Group Project/5v_cleandf.rdata/")
+load("5v_cleandf.rdata")
+View(df)
+write.csv(signalList, "5v_cleandf.csv")
+
+View(signalList)
