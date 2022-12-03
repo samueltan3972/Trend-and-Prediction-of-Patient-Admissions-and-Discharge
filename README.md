02dfbfbf002b535a6651ae3eb3e24338a86b397c
Samuel Tan <31382683+samueltan3972@users.noreply.github.com>
2022-12-03 16:45:44 +0800

    add init dataset

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Getting Started
 
-This is the repository for UM WQD 7003 Data Analytics Group Project.
+This is the repository for <b>UM WQD 7003 Data Analytics</b> Group Project.
+
+Emergency Overcrowding Issue:
+
+
+
+Bed Management:
+1. given patient's data, how long would that patient occupy a bed?
+2. EDA - trend of department occupancy
--- convert.R
+++ convert.R
@@ -1,6 +1,12 @@
 setwd("D:/UM Master/Sem 1/Data Analytics/Group Project/5v_cleandf.rdata/")
 load("5v_cleandf.rdata")
+summary(df)
+#write.csv(signalList, "5v_cleandf.csv")
+summary
+type(df)
 View(df)
-write.csv(signalList, "5v_cleandf.csv")
 
-View(signalList)
+#write.csv(df, "5v_cleandf.csv")
+
+write.table(names(df), "column_name.csv", sep = "\n")
+class(names(df))
